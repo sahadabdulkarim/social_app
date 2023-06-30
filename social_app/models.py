5b9a9de4aabcc3de9e0f75e4bfeb3d8f86385e7e
PP Sahad Abdul Karim <ppsahadabdulkarim@gmail.com>
2023-07-01 00:11:30 +0530

    models added

--- social_app/models.py
+++ social_app/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    profile_picture = models.ImageField(upload_to="profile_pics/")
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    bio = models.TextField(max_length=200, blank=True)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.TextField(blank=True)
+    image_or_video_content = models.ImageField(upload_to="posts/")
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Post - {self.id}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.user.username}'s post"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s comment on {self.post.user.username}'s post"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.tag.name} tag on {self.post.user.username}'s post"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, related_name="follower", on_delete=models.CASCADE
+    )
+    following = models.ForeignKey(
+        User, related_name="following", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.follower.username} is following {self.following.username}"
